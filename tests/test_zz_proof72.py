e196e91fbe3eb14bd1e7f52e0113416b3e0a161e
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-07-23 01:34:15 +0200

    Revert "QA PROOF 3 for #72, reverted by the next commit: a new test file, unlisted"
    This reverts commit 8d58b0c291f2a7e87b2b2190456320887a38bf4f.

--- tests/test_zz_proof72.py
+++
@@ -1,4 +0,0 @@
-# QA PROOF for #72, reverted by the next commit: a new test file added to the
-# tree but not listed in TEST_FILES. Adding a file must fail until it is listed.
-def test_proof72_new_file_not_in_the_manifest() -> None:
-    assert True
